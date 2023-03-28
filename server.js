1ce3e91bde55223e95387cf33acf5a90699c18ed
Tifannie Truman <nietru143@gmail.com>
2023-03-28 13:22:54 -0400

    port fixed

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const uuid = require("uuid");
 const notes = require("./db/db.json");
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
